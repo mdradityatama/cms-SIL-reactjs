f26c9bf51a3066c62d19ce41eae5cfdff04ed4f6
mdradityatama <mdimasradityatama@gmail.com>
2019-12-18 14:00:44 +0700

    connection firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDtvsI7BnGukE_wUThiiVTQHRB3WH1YGX0",
+  authDomain: "pejuang-mahasiswa.firebaseapp.com",
+  databaseURL: "https://pejuang-mahasiswa.firebaseio.com",
+  projectId: "pejuang-mahasiswa",
+  storageBucket: "pejuang-mahasiswa.appspot.com",
+  messagingSenderId: "1024899497258",
+  appId: "1:1024899497258:web:d855c4fece1e43fcef3253",
+  measurementId: "G-PSZH4D4PGP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
