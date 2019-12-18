bc110acbbc0ff3539e338222793173085c78bc53
mdradityatama <mdimasradityatama@gmail.com>
2019-12-18 16:16:35 +0700

    create register user

---
+++ src/containers/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render(){
+    return(
+      <div>
+        <p>Login page</p>
+        <button>Go to Register</button>
+        <button>Go To Dashboard</button>
+      </div>
+    )
+  }
+}
+
+export default Login;
